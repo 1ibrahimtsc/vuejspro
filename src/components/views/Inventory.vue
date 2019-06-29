2b1ff78ee24de633cedf76f8edc86abf752fb25c
1ibrahimtsc <1ibrahimtsc@gmail.com>
2019-06-29 12:07:55 +0600

    30 completed

--- src/components/views/Inventory.vue
+++ src/components/views/Inventory.vue
@@ -1,7 +1,7 @@
 
 
 <template>
-        <div class="row">
+        <div v-if="!loading" class="row">
                 <div v-for="(item, index) in items" class="card" style="width: 17rem;">
                   <img class="card-img-top" :src="item.photo" alt="Card image cap">
                   <div class="card-body">
@@ -9,18 +9,16 @@
                     <p class="card-text"> {{ item.price }} </p>
                     <a @click="addToCart(item)" class="btn btn-primary"> + Add </a>
                   </div>
-                </div>
-
-                
-
-              
+                </div>              
             </div>
+            <h1 v-else>Loading...</h1>
 </template>
 <script>
 import axios from 'axios'
 export default {
 data(){
   return {
+    loading: true,
     items: []
   }
 },
@@ -33,7 +31,8 @@ methods: {
   },
   fechInventory(){
      axios.get('http://localhost:3000/items').then(response => {
-     this.items = response.data
+        this.items = response.data
+        this.loading = false        
    })
   }
  }
